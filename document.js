d39074bb4d5645b0d940fff761583fc51311e1bb
Stef Busking <stef.busking@gmail.com>
2012-10-26 16:40:20 +0200

    Implemented Document.documentElement and made Document inherit from Node properly.

--- document.js
+++ document.js
@@ -1,21 +1,30 @@
 define(['./node', './element', './text', './selection/range'], function(Node, Element, Text, Range) {
 	// Document base
 	function Document() {
-		this.root = new Node('root');
+		Node.call(this, 'document');
+
+		this.appendChild(new Node('root'));
+		this.documentElement = this.firstChild;
 	}
 	Document.prototype = new Node('document');
 	Document.prototype.constructor = Document;
 
+	// Override replaceChild to update the documentElement reference
+	Document.prototype.replaceChild = function(newChild, oldChild) {
+		Node.prototype.replaceChild.call(this, newChild, oldChild);
+		this.documentElement = this.firstChild;
+	};
+
 	// Creates a new element with the given tag name.
 	Document.prototype.createElement = function(name) {
-		var node = new ElementNode(name);
+		var node = new Element(name);
 		node.ownerDocument = this;
 		return node;
 	};
 
 	// Creates a text node.
 	Document.prototype.createTextNode = function(content) {
-		var node = new TextNode(content);
+		var node = new Text(content);
 		node.ownerDocument = this;
 		return node;
 	};
