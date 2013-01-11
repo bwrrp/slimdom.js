e38c7a2cee9a4d19ba8f8e9a7aae6fa0dd1173fe
Tim Schlechter <timschlechter@gmail.com>
2013-01-11 14:47:03 +0100

    Added attribute getter and setter methods.

--- element.js
+++ element.js
@@ -7,5 +7,23 @@ define(['./node'], function(Node) {
 	Element.prototype = new Node(Node.ELEMENT_NODE);
 	Element.prototype.constructor = Element;
 
+	Element.prototype.getAttributeValue = function(attributeName) {
+		if (!this.attributes)
+			return;
+
+		return this.attributes[attributeName];
+	}
+
+	Element.prototype.setAttributeValue = function(attributeName, attributeValue) {
+		if (!this.attributes)
+			this.attributes = {};
+
+
+		// TODO: remove if value undefined
+
+		// TODO: waarde altijd als string? Dus toString() nodig? Voorkomt wel onduidelijkheid
+		this.attributes[attributeName] = '' + attributeValue;
+	}
+
 	return Element;
 });
