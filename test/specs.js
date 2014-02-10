941e554759b17e13366c9448548f8ae1cbeb0262
Stef Busking <stef.busking@gmail.com>
2014-02-10 17:05:02 +0100

    Update case for test suite files.

--- test/specs.js
+++ test/specs.js
@@ -1,11 +1,11 @@
 define(
 	[
-		'./specs/document.tests',
-		'./specs/element.tests',
-		'./specs/text.tests',
-		'./specs/processinginstruction.tests',
-		'./specs/range.tests',
-		'./specs/mutationobserver.tests'
+		'./specs/Document.tests',
+		'./specs/Element.tests',
+		'./specs/Text.tests',
+		'./specs/ProcessingInstruction.tests',
+		'./specs/Range.tests',
+		'./specs/MutationObserver.tests'
 	],
 	function() {
 	}
