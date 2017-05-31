83de8b6735973d5e0109b51ef52593a2f2478847
Stef Busking <stef.busking@gmail.com>
2017-05-31 10:54:12 +0200

    Export all public types.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { implementation } from './DOMImplementation';
 import XMLDocument from './XMLDocument';
 
+export { default as Attr } from './Attr';
+export { default as CharacterData } from './CharacterData';
+export { default as Comment } from './Comment';
+export { default as Document } from './Document';
+export { default as DocumentType } from './DocumentType';
+export { default as DOMImplementation } from './DOMImplementation';
+export { default as Element } from './Element';
 export { implementation } from './DOMImplementation';
 export { default as Node } from './Node';
+export { default as ProcessingInstruction } from './ProcessingInstruction';
 export { default as Range } from './Range';
+export { default as Text } from './Text';
+export { default as XMLDocument } from './XMLDocument';
 export { default as MutationObserver } from './mutation-observer/MutationObserver';
 
 export function createDocument (): XMLDocument {
