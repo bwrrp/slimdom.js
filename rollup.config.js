a3825b43ff624d1f33f6304a6c49a405468a9c37
Stef Busking <stef.busking@gmail.com>
2022-06-23 10:07:35 +0200

    Fix ESM usage in node

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,17 @@ import resolve from '@rollup/plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 
-const { main: MAIN_DEST_FILE, module: MODULE_DEST_FILE } = require('./package.json');
+const {
+	exports: {
+		'.': { require: CJS_DEST_FILE, import: ESM_DEST_FILE },
+	},
+} = require('./package.json');
 
 export default {
 	input: 'lib/index.js',
 	output: [
-		{ name: 'slimdom', file: MAIN_DEST_FILE, format: 'umd', exports: 'named', sourcemap: true },
-		{ file: MODULE_DEST_FILE, format: 'es', sourcemap: true },
+		{ name: 'slimdom', file: CJS_DEST_FILE, format: 'umd', exports: 'named', sourcemap: true },
+		{ file: ESM_DEST_FILE, format: 'es', sourcemap: true },
 	],
 	external: [],
 	onwarn(warning) {
